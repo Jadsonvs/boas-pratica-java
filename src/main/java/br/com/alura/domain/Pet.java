580c734ca6e9eb853a373aee0c0abe2f1fd21da4
Jadson Viana <jadson.vsantos2013@gmail.com>
2024-06-12 22:49:20 -0300

    Criando objeto Pet

--- src/main/java/br/com/alura/domain/Pet.java
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -1,4 +1,68 @@
 package br.com.alura.domain;
 
 public class Pet {
+
+    private String nome;
+    private int idade;
+    private String tipo;
+    private String raca;
+    private String cor;
+    private Float peso;
+
+    public Pet(String nome, int idade, String tipo, String raca, String cor, Float peso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+        this.raca = raca;
+        this.cor = cor;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Float peso) {
+        this.peso = peso;
+    }
 }
